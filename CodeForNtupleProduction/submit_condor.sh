103f80161de4b1b93063bb716525ecdf8261f65a
Jing Wang <jwang@hidsk0001.cmsaf.mit.edu>
2015-09-02 04:19:56 -0400

    up

--- CodeForNtupleProduction/submit_condor.sh
+++ CodeForNtupleProduction/submit_condor.sh
@@ -6,30 +6,32 @@
 
 ###Plain root .C to be run
 #CONFIGFILE="loop.C"
-CONFIGFILE="loop.C"
+CONFIGFILE="loopnew.C"
 
 ###All the header/related files needed
 #TRANSFERFILE="loop.C,loop.h"
-TRANSFERFILE="loop.C,loop.h"
+TRANSFERFILE="loopnew.C,loopnew.h"
 
 ###Folder location within which files are to be run
-DATASET=/mnt/hadoop/cms/store/user/tawei/Dfinder/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/Pyquen_D0tokaonpion_Pt0_D0pt1p0_TuneZ2_Unquenched_2760GeV_step3_20150612_250kevt_20150824_kpi/*
+#DATASET=/mnt/hadoop/cms/store/user/twang/Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV/DinderMC_Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV_KpPim_20150830/5d741c977c01918c6417913dff2dce5a/*
+DATASET=/mnt/hadoop/cms/store/user/twang/Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV/DinderMC_Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV_KmPip_20150830/4cfff3e2677f7ce8b808cc4625d49a9e/*
 
 ###Output file location
-DESTINATION=/mnt/hadoop/cms/store/user/jwang/Dmeson/ntD_20150829_Pyquen_D0tokaonpion_Pt0_D0pt1p0_TuneZ2_Unquenched_2760GeV_step3_20150612_250kevt_20150824_kpi_EvtBase
-#DESTINATION=/mnt/hadoop/cms/store/user/jwang/Dmeson/ntD_20150828_Pyquen_test
+#DESTINATION=/mnt/hadoop/cms/store/user/jwang/Dmeson/ntD_20150902_Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV_KpPim_20150830_EvtBase
+DESTINATION=/mnt/hadoop/cms/store/user/jwang/Dmeson/ntD_20150902_Pyquen_D0tokaonpion_D0pt1p0_Pthat0_TuneZ2_Unquenched_2760GeV_KmPip_20150830_EvtBase
+
 ###Output file name
-OUTFILE="ntD_20150829_mc_evtbase"
-#OUTFILE="ntD_20150828_mc_test"
+#OUTFILE="ntD_20150902_mc_KpPim_evtbase"
+OUTFILE="ntD_20150902_mc_KmPip_evtbase"
 
 ###Maximum number of files to be run
 MAXFILES=1000
 
 ###Log file location and it's name
-LOGDIR=/export/d00/scratch/jwang/hadooplogs/ntD_20150829_mc_evtbase_logs
-LOGNAME=log_nt_20140829_mc_evtbase
-#LOGDIR=/export/d00/scratch/jwang/nt_20140708_kp_pPb_evtbase_logs
-#LOGNAME=log_nt_20140708_kp_pPb_evtbase
+#LOGDIR=/export/d00/scratch/jwang/hadooplogs/ntD_20150902_mc_KpPim_evtbase_logs
+LOGDIR=/export/d00/scratch/jwang/hadooplogs/ntD_20150902_mc_KmPip_evtbase_logs
+#LOGNAME=log_ntD_20150902_mc_KpPim_evtbase
+LOGNAME=log_ntD_20150902_mc_KmPip_evtbase
 
 ########################## Create subfile ###############################
 dateTime=$(date +%Y%m%d%H%M)
