3e602330d9f7100d3e0fa432011a06d7fd92ebed
boundino <boundino@gmail.com>
2015-10-29 10:22:07 -0400

    add pp loophlt&evtmatching macros

--- EvtMatching/evtmatchingpp.C
+++ EvtMatching/evtmatchingpp.C
@@ -6,46 +6,37 @@
 #include <fstream>
 #include "evtmatching.h"
 
-Float_t pthat = 15;
-int evtmatching(TString infdfinder=Form("/export/d00/scratch/jwang/Dmeson/evtmatchingInput/DfinderMC_Pyquen_D0tokaonpion_D0pt%.0fp0_Pthat%.0f_TuneZ2_Unquenched_5020GeV_GENSIM_75x_v2_20151027.root",pthat,pthat), TString infhlt=Form("/export/d00/scratch/jwang/Dmeson/evtmatchingInput/openHLT_HF_HLTHeavyFlavour_MVA_Pyquen_D0tokaonpion_D0pt%.0fp0_Pthat%.0f_1026.root",pthat,pthat),TString outfile=Form("/export/d00/scratch/jwang/Dmeson/DfinderMC_20151028_EvtMatching_Pyquen_D0tokaonpion_D0pt%.0fp0_Pthat%.0f_TuneZ2_Unquenched_5020GeV_GENSIM_75x_v2_20151027.root",pthat,pthat))
+Float_t pthat = 5;
+int evtmatchingpp(TString infdfinder=Form("/export/d00/scratch/jwang/Dmeson/evtmatchingInput/DfinderMC_Pythia_D0pt%.0fp0_Pthat%.0f_TuneZ2_5020GeV_GENSIM_75x_1015_20151027.root",pthat,pthat), TString infhlt=Form("/export/d00/scratch/jwang/Dmeson/evtmatchingInput/openHLT_HF_HLTHeavyFlavour_MVA_Pythia_D0tokaonpion_D0pt%.0fp0_Pthat%.0f_1026.root",pthat,pthat),TString outfile=Form("/export/d00/scratch/jwang/Dmeson/DfinderMC_20151029_EvtMatching_Pythia_D0pt%.0fp0_Pthat%.0f_TuneZ2_5020GeV_GENSIM_75x_1015_20151027.root",pthat,pthat))
 {
   TFile* fdfinder = new TFile(infdfinder);
   TFile* fhlt = new TFile(infhlt);
   TTree* root = (TTree*)fdfinder->Get("Dfinder/root");
-  TTree* hiroot = (TTree*)fdfinder->Get("hiEvtAnalyzer/HiTree");
   TTree* hltroot = (TTree*)fhlt->Get("hltbitanalysis/HltTree");
 
   TFile* outf = new TFile(outfile,"recreate");
   TDirectory* droot = outf->mkdir("Dfinder","");
   TDirectory* dhltroot = outf->mkdir("hltbitanalysis","");
-  TDirectory* dhiroot = outf->mkdir("hiEvtAnalyzer","");
   droot->cd();
   TTree* ntReco = root->CloneTree(0);
   dhltroot->cd();
   TTree* ntHlt = hltroot->CloneTree(0);
-  dhiroot->cd();
-  TTree* ntHi = hiroot->CloneTree(0);
 
   setEvtDBranch(root);
   setEvtHLTBranch(hltroot);
-  setEvtHIBranch(hiroot);
   cout<<"---- Check evt no. for three trees"<<endl;
-  cout<<root->GetEntries()<<", "<<hltroot->GetEntries()<<", "<<hiroot->GetEntries()<<endl;
+  cout<<root->GetEntries()<<", "<<hltroot->GetEntries()<<endl;
   Long64_t nentriesReco = root->GetEntries();
   Long64_t nentriesHlt = hltroot->GetEntries();
-  Long64_t nentriesHi = hiroot->GetEntries();
   Int_t aRecoRunNo[nentriesReco],aRecoEvtNo[nentriesReco],aRecoLumiNo[nentriesReco];
   Int_t aHltRun[nentriesHlt],aHltEvent[nentriesHlt],aHltLumiBlock[nentriesHlt];
-  Int_t aHiRun[nentriesHi],aHiEvt[nentriesHi],aHiLumi[nentriesHi];
+
   cout<<"---- Event reading"<<endl;
-  Long64_t nentriesMax;
-  if(nentriesHi>((nentriesReco>nentriesHlt)?nentriesReco:nentriesHlt)) nentriesMax=nentriesHi;
-  else nentriesMax=((nentriesReco>nentriesHlt)?nentriesReco:nentriesHlt);
-  for(Int_t i=0;i<nentriesMax;i++)
+  for(Int_t i=0;i<((nentriesReco>nentriesHlt)?nentriesReco:nentriesHlt);i++)
     {
       if(i%10000==0)
 	{
-	  cout<<setw(7)<<i<<" / "<<nentriesMax<<endl;
+	  cout<<setw(7)<<i<<" / "<<((nentriesReco>nentriesHlt)?nentriesReco:nentriesHlt)<<endl;
 	}
       if(i<nentriesReco)
 	{
@@ -61,23 +52,15 @@ int evtmatching(TString infdfinder=Form("/export/d00/scratch/jwang/Dmeson/evtmat
 	  aHltEvent[i] = (Int_t)HltEvent;
 	  aHltLumiBlock[i] = HltLumiBlock;
 	}
-      if(i<nentriesHi)
-	{
-	  hiroot->GetEntry(i);
-	  aHiRun[i] = HiRun;
-	  aHiEvt[i] = HiEvt;
-	  aHiLumi[i] = HiLumi;
-	}
     }
 
-  Int_t hltmatching[nentriesReco],himatching[nentriesReco];
+  Int_t hltmatching[nentriesReco];
   ofstream fout("evtmatchingResult/evtmatching.dat");
   cout<<"---- Event matching"<<endl;
   for(Int_t ievt=0;ievt<nentriesReco;ievt++)
     {
       if(ievt%10000==0) cout<<setw(7)<<ievt<<" / "<<nentriesReco<<endl;
       hltmatching[ievt] = -1;
-      himatching[ievt] = -1;
       for(Int_t jevt=0;jevt<nentriesHlt;jevt++)
 	{
 	  if(aRecoRunNo[ievt]==aHltRun[jevt]&&aRecoEvtNo[ievt]==aHltEvent[jevt]&&aRecoLumiNo[ievt]==aHltLumiBlock[jevt])
@@ -86,21 +69,13 @@ int evtmatching(TString infdfinder=Form("/export/d00/scratch/jwang/Dmeson/evtmat
 	      break;
 	    }
 	}
-      for(Int_t jevt=0;jevt<nentriesHi;jevt++)
-	{
-	  if(aRecoRunNo[ievt]==aHiRun[jevt]&&aRecoEvtNo[ievt]==aHiEvt[jevt]&&aRecoLumiNo[ievt]==aHiLumi[jevt])
-	    {
-	      himatching[ievt] = jevt;
-	      break;
-	    }
-	}
-      fout<<hltmatching[ievt]<<" "<<himatching[ievt]<<endl;
+      fout<<hltmatching[ievt]<<endl;
     }
   cout<<"---- Event matching done"<<endl;
   cout<<"---- Writing hlt tree"<<endl;
   for(Int_t i=0;i<nentriesReco;i++)
     {
-      if (i%1000==0) cout<<setw(7)<<i<<" / "<<nentriesReco<<"  HI index:"<<setiosflags(ios::left)<<setw(7)<<himatching[i]<<" HLT index:"<<hltmatching[i]<<endl;
+      if (i%1000==0) cout<<setw(7)<<i<<" / "<<nentriesReco<<" HLT index:"<<setiosflags(ios::left)<<hltmatching[i]<<endl;
       if(hltmatching[i]<0)
 	{
 	  continue;
@@ -108,9 +83,6 @@ int evtmatching(TString infdfinder=Form("/export/d00/scratch/jwang/Dmeson/evtmat
       root->GetEntry(i);
       droot->cd();
       ntReco->Fill();
-      hiroot->GetEntry(himatching[i]);
-      dhiroot->cd();
-      ntHi->Fill();
       hltroot->GetEntry(hltmatching[i]);
       dhltroot->cd();
       ntHlt->Fill();
