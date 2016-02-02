a150ea136c8a374e93c8ca5e7ce537fc73a1c684
Jing Wang <boundino@gmail.com>
2016-02-02 20:31:47 +0100

    add pbpb sample location

--- weighPthat/weighPthat.sh
+++ weighPthat/weighPthat.sh
@@ -2,6 +2,7 @@
 
 ###
 
+##pp
 #Dzero
 sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={7.085e+09,1.593e+08,9.881e+06,1.975e+06,9.484e+04,9.144e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
 INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0_noweight.root";
@@ -16,6 +17,22 @@ OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_
 #sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.481e+07,6.207e+06,1.252e+06,6.522e+04,6.072e+03,0.}; int genSignal[2]={11,12};' weighPthat.C > weighPthat_tmp.C
 #INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpipipi_noweight.root";
 #OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpipipi_pthatweight.root";
+
+##pbpb
+#Dzero
+#sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={7.085e+09,1.593e+08,9.881e+06,1.975e+06,9.484e+04,9.144e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
+#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi/ntD_EvtBase_20160127_Dfinder_20151229_Pythia8_prompt_D0_noweight.root";
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi/ntD_EvtBase_20160127_Dfinder_20151229_Pythia8_prompt_D0_pthatweight.root";
+
+#Dstar 3p
+#sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.349e+07,6.291e+06,1.230e+06,6.469e+04,6.646e+03,0.}; int genSignal[2]={9,10};' weighPthat.C > weighPthat_tmp.C
+#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dstar_D0kpi/ntD_EvtBase_20160104_Dfinder_20151229_PbPb_Pythia8D0kpi_Dstar_noweight.root";
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dstar_D0kpi/ntD_EvtBase_20160104_Dfinder_20151229_PbPb_Pythia8D0kpi_Dstar_pthatweight.root";
+
+#Dstar 5p
+#sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.481e+07,6.207e+06,1.252e+06,6.522e+04,6.072e+03,0.}; int genSignal[2]={11,12};' weighPthat.C > weighPthat_tmp.C
+#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dstar_D0kpipipi/ntD_EvtBase_20160104_Dfinder_20151229_PbPb_Pythia8D0kpipipi_Dstar_noweight.root";
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dstar_D0kpipipi/ntD_EvtBase_20160104_Dfinder_20151229_PbPb_Pythia8D0kpipipi_Dstar_pthatweight.root";
 
 ###
 
