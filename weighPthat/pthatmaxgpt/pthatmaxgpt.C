a8a2ea786640f1209c1d81bf9e042df90d24b5f3
Jing Wang <boundino@gmail.com>
2016-02-04 15:59:13 +0100

    update weighPthat

--- weighPthat/pthatmaxgpt/pthatmaxgpt.C
+++ weighPthat/pthatmaxgpt/pthatmaxgpt.C
@@ -1,68 +1,31 @@
 #include "uti.h"
 #define MAX_GEN      6000
 
-void pthatmaxgpt(Float_t samplePthat=0, Bool_t toPlot=false)
+void pthatmaxgpt(Bool_t ifweigh=true)
 {
-  const int nBins=6; Double_t ptBins[nBins+1]={0.,5.,10.,15.,30.,50.,100.};
-  TFile* inf = new TFile(Form("/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0pt%.0fp0_Pthat%.0f_TuneCUETP8M1_5020GeV_evtgen130_GEN_SIM_20151212_dPt1tkPt1_D0Ds.root",samplePthat,samplePthat));
-  TTree* ntGen = (TTree*)inf->Get("ntGen");
+  const int nBins=50; Float_t minBin=0,maxBin=100;  TFile* inf = new TFile("/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0_pthatweight.root");
+  //const int nBins=50; Float_t minBin=0,maxBin=60;  TFile* inf = new TFile("/data/wangj/debug/weighPthat/addmaxGenptBranch/Dzero/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0_addmaxgenpt_0.root");
   TTree* ntHi = (TTree*)inf->Get("ntHi");
 
-  cout<<ntGen->GetEntries()<<" "<<ntHi->GetEntries()<<endl;
-  Int_t nentries = ntGen->GetEntries();
-  TH2D* h = new TH2D("h",";Pthat;Max Gen p_{T} (GeV/c)",nBins,ptBins,nBins,ptBins);
-
-  Int_t Gsize;              ntGen->SetBranchAddress("Gsize",&Gsize);
-  Int_t GisSignal[MAX_GEN]; ntGen->SetBranchAddress("GisSignal",GisSignal);
-  Float_t Gpt[MAX_GEN];     ntGen->SetBranchAddress("Gpt",Gpt);
-  Float_t pthat;            ntHi->SetBranchAddress("pthat",&pthat);
-  Float_t qScale;           ntHi->SetBranchAddress("qScale",&qScale);
+  Int_t nentries = ntHi->GetEntries();
+  TH2D* h = new TH2D("h",";Pthat;Max Gen p_{T} (GeV/c)",nBins,minBin,maxBin,nBins,minBin,maxBin);
+  h->SetMarkerSize(0.5);
+  Float_t pthatweight;
+  if(ifweigh) ntHi->SetBranchAddress("pthatweight",&pthatweight);
+  Float_t pthat;       ntHi->SetBranchAddress("pthat",&pthat);
+  Float_t maxDgenpt;   ntHi->SetBranchAddress("maxDgenpt",&maxDgenpt);
   for(Int_t i=0;i<nentries;i++)
     {
-      ntGen->GetEntry(i);
       ntHi->GetEntry(i);
-      Float_t maxpt=0;
-      for(Int_t j=0;j<Gsize;j++)
-        {
-          if(!(GisSignal[j]==9||GisSignal[j]==10)) continue;
-          if(Gpt[j]>maxpt) maxpt=Gpt[j];
-        }
-      h->Fill(pthat,maxpt);
+      if(ifweigh) h->Fill(pthat,maxDgenpt,pthatweight);
+      else h->Fill(pthat,maxDgenpt);
     }
 
   cout<<endl;
-  cout<<"             Pthat"<<samplePthat<<" Sample"<<endl;
-  cout<<"            ╒"<<"═══════════"<<"╤"<<"═══════════"<<"╤"<<"═══════════"<<"╤"<<"═══════════"<<"╤"<<"═══════════"<<"╤"<<"═══════════"<<"╤"<<"═══════════"<<"╕"<<endl;
-  for(Int_t i=0;i<nBins;i++)
-    {
-      if(i!=0) cout<<"            ├"<<"───────────"<<"┼"<<"───────────"<<"┼"<<"───────────"<<"┼"<<"───────────"<<"┼"<<"───────────"<<"┼"<<"───────────"<<"┼"<<"───────────"<<"┤"<<endl;
-      if(i==3) cout<<" Max Gen pT ";
-      else cout<<"            ";
-      cout<<"| "<<setiosflags(ios::left)<<setw(10)<<Form("%.0f - %.0f",ptBins[nBins-i-1],ptBins[nBins-i]);
-      for(Int_t j=0;j<nBins;j++)
-        {
-          cout<<"| "<<setiosflags(ios::left)<<setw(10)<<h->GetBinContent(j+1,nBins-i);
-        }
-      cout<<"|"<<endl;
-    }
-  cout<<"            ╞"<<"═══════════"<<"╪"<<"═══════════"<<"╪"<<"═══════════"<<"╪"<<"═══════════"<<"╪"<<"═══════════"<<"╪"<<"═══════════"<<"╪"<<"═══════════"<<"╡"<<endl;
-  cout<<"            |           ";
-  for(Int_t j=0;j<nBins;j++)
-    {
-      cout<<"| "<<setiosflags(ios::left)<<setw(10)<<Form("%.0f - %.0f",ptBins[j],ptBins[j+1]);
-    }
-  cout<<"|"<<endl;
-  cout<<"            ╘"<<"═══════════"<<"╧"<<"═══════════"<<"╧"<<"═══════════"<<"╧"<<"═══════════"<<"╧"<<"═══════════"<<"╧"<<"═══════════"<<"╧"<<"═══════════"<<"╛"<<endl;
-  cout<<"                                                   Pthat"<<endl;
-  cout<<endl;
-
-  if(toPlot)
-    {
-      gStyle->SetPadLeftMargin(0.17);
-      gStyle->SetPadRightMargin(0.20);
-      TCanvas* c = new TCanvas("c","",600,600);
-      h->Draw("colz");
-    }
+  gStyle->SetPadLeftMargin(0.17);
+  gStyle->SetPadRightMargin(0.15);
+  TCanvas* c = new TCanvas("c","",600,600);
+  c->SetLogz();
+  h->Draw("colz");
+  c->SaveAs("pthatmaxgpt.pdf");
 }
-
-//TFile* inf = new TFile("/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpi_noweight.root");
--- weighPthat/weighPthat.sh
+++ weighPthat/weighPthat.sh
@@ -5,18 +5,23 @@
 ##pp
 #Dzero
 sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={7.085e+09,1.593e+08,9.881e+06,1.975e+06,9.484e+04,9.144e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
-INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0_noweight.root";
-OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160125_Dfinder_20151229_pp_Pythia8_prompt_D0_pthatweight.root";
+INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8_prompt_D0_dPt0tkPt0p5_noweight.root";
+OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8_prompt_D0_dPt0tkPt0p5_pthatweight.root";
 
 #Dstar 3p
 #sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.349e+07,6.291e+06,1.230e+06,6.469e+04,6.646e+03,0.}; int genSignal[2]={9,10};' weighPthat.C > weighPthat_tmp.C
-#INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpi_noweight.root";
-#OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpi_pthatweight.root";
+#INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8D0kpi_dPt0tkPt0p5_noweight.root";
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8D0kpi_dPt0tkPt0p5_pthatweight.root";
 
 #Dstar 5p
 #sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.481e+07,6.207e+06,1.252e+06,6.522e+04,6.072e+03,0.}; int genSignal[2]={11,12};' weighPthat.C > weighPthat_tmp.C
-#INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpipipi_noweight.root";
-#OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160112_Dfinder_20151229_pp_Pythia8D0kpipipi_pthatweight.root";
+#INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8D0kpipipi_dPt0tkPt0p5_noweight.root";
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dstar_D0kpipipi/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8D0kpipipi_dPt0tkPt0p5_pthatweight.root";
+
+#Dzero nonprompt
+#sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={2.370e+08,1.281e+07,1.318e+06,3.082e+05,1.662e+04,1.371e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
+#INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_nonprompt/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8_nonprompt_D0_noweight.root"
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_nonprompt/ntD_EvtBase_20160203_Dfinder_20160201_pp_Pythia8_nonprompt_D0_pthatweight.root"
 
 ##pbpb
 #Dzero
