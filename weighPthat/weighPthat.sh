fe9b8511a009ff330cdfac6c975cbce809843160
Jing Wang <boundino@gmail.com>
2016-03-02 23:25:38 +0100

    update weighPthat inputfile

--- weighPthat/weighPthat.sh
+++ weighPthat/weighPthat.sh
@@ -5,8 +5,8 @@
 ###pp
 #Dzero
 sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={6.885e+09,1.581e+08,9.830e+06,1.916e+06,9.793e+04,9.066e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
-INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160229_Dfinder_20160215_pp_Pythia8_prompt_D0_dPt0tkPt0p5_noweight.root"
-OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160229_Dfinder_20160215_pp_Pythia8_prompt_D0_dPt0tkPt0p5_pthatweight.root"
+INPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi_prompt/ntD_EvtBase_20160229_Dfinder_20160215_pp_Pythia8_prompt_D0_dPt0tkPt0p5_noweight.root"
+OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi_prompt/ntD_EvtBase_20160229_Dfinder_20160215_pp_Pythia8_prompt_D0_dPt0tkPt0p5_pthatweight.root"
 
 #Dzero nonprompt
 #sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={1.895e+08,1.485e+07,1.424e+06,2.948e+05,1.520e+04,1.369e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
@@ -27,13 +27,13 @@ OUTPUTFILE="/data/wangj/MC2015/Dntuple/pp/ntD_pp_Dzero_kpi/ntD_EvtBase_20160229_
 ###pbpb
 #Dzero
 #sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={6.885e+09,1.581e+08,9.830e+06,1.916e+06,9.793e+04,9.066e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
-#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_prompt_D0_dPt1tkPt1_noweight.root"
-#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_prompt_D0_dPt1tkPt1_pthatweight.root"
+#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi_prompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_prompt_D0_dPt1tkPt1_noweight.root"
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi_prompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_prompt_D0_dPt1tkPt1_pthatweight.root"
 
 #Dzero nonprompt
 #sed '1iconst int nBins=6; float pthatBin[nBins]={0,5,10,15,30,50}; float crosssec[nBins+1]={1.895e+08,1.485e+07,1.424e+06,2.948e+05,1.520e+04,1.369e+03,0.}; int genSignal[2]={1,2};' weighPthat.C > weighPthat_tmp.C
-#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_pp_Dzero_kpi_nonprompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_nonprompt_D0_dPt1tkPt1_noweight.root"
-#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_pp_Dzero_kpi_nonprompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_nonprompt_D0_dPt1tkPt1_pthatweight.root"
+#INPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi_nonprompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_nonprompt_D0_dPt1tkPt1_noweight.root"
+#OUTPUTFILE="/data/wangj/MC2015/Dntuple/PbPb/ntD_PbPb_Dzero_kpi_nonprompt/ntD_EvtBase_20160229_Dfinder_20160215_PbPb_Pythia8_nonprompt_D0_dPt1tkPt1_pthatweight.root"
 
 #Dstar 3p
 #sed '1iconst int nBins=5; float pthatBin[nBins]={5,10,15,30,50}; float crosssec[nBins+1]={9.440e+07,6.280e+06,1.239e+06,6.518e+04,6.115e+03,0.}; int genSignal[2]={9,10};' weighPthat.C > weighPthat_tmp.C
